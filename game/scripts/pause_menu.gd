7a1eeaf221b09a88e550b6dddd750173c2e3510b
Mikael Lind <elemel@elemel.se>
2026-04-18 14:57:50 +0200

    Detect pausing on web

--- game/scripts/pause_menu.gd
+++ game/scripts/pause_menu.gd
@@ -9,6 +9,11 @@ func _ready() -> void:
 	pause()
 
 
+func _physics_process(_delta: float) -> void:
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE and not get_tree().paused:
+		pause()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
